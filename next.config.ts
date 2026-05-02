e8ceace8acc1781561a25f2479fbc5e8ad8b3b86
CAP1974 <carlospinto08@gmail.com>
2026-05-02 22:56:45 +0100

    Add /publish redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/publish',
+        destination: '/publish.html',
+        permanent: false,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
